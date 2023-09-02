8cf33f857440b8b07ae39317f2465007557cef88
Inked Logic <140082051+inked-logic@users.noreply.github.com>
2023-09-02 13:11:38 +0100

    Create the function multiply
    multiply returns the multiplication two opernads

--- javascript-fundamentals/javascript.js
+++ javascript-fundamentals/javascript.js
@@ -1,5 +1,16 @@
+// The fisrt assignment
+
 function add7(number) {
   return number + 7;
 }
 
 console.log(add7(5));
+
+// The second assignment
+
+function multiply(operand1, operand2) {
+  return operand1 * operand2;
+}
+
+console.log(multiply(5,3));
+
