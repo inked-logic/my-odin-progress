3b680fc390e080956f83c7eb9fa37c186a7fca0b
Inked Logic <140082051+inked-logic@users.noreply.github.com>
2023-09-06 12:04:46 +0100

    Create the main algorithme for the game

---
+++ foundations-rock-paper-scissors/script.js
@@ -0,0 +1,9 @@
+// rock-paper-scissors
+// ask the user for a string as an input, must be one of the three possible choices (rock, paper, scissors)
+// Generate a random choice from the possible choices using the function explained below
+// getComputerChoice => a function that returns a random choice [String] from (rock, paper, scissors)
+// Compare the user's choice with the random choice returned by getComputerChoice and return the winner as a string
+// play a total of 5 rounds while keeping counting the number of rounds won by each player
+// display the results
+// ask the user if he wants to play again (yes or no question)
+// if yes replay, if no exit
